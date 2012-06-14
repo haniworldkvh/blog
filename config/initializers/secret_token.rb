3f053b09eb9e2e2c61b72023c09b9950e264d92c
haniworld <haruka.higuchi@kvh.co.jp>
2012-06-14 12:15:34 +0900

    First commit rails skelton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '6fab01ec7199203cce9ada816e69f33e9c157e0947b7305ea2c2db7ee40594ff30e7b779649f85eaab1acfc44ad66d08bc323bda26c90e18000ad5745a742253'
